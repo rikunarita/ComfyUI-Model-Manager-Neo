fc8f960c3152788e8699a73b4a4610c41a209b82
成田璃久 <rikunarita7669@gmail.com>
2026-09-01 02:05:21 +0900

    Change ComfyApp export to use 'as any' type

--- src/scripts/comfyAPI.ts
+++ src/scripts/comfyAPI.ts
@@ -3,6 +3,6 @@ export const api = window.comfyAPI.api.api
 
 export const $el = window.comfyAPI.ui.$el
 
-export const ComfyApp = window.comfyAPI.app.ComfyApp
+export const ComfyApp = window.comfyAPI.app as any
 export const ComfyButton = window.comfyAPI.button.ComfyButton
 export const ComfyDialog = window.comfyAPI.dialog.ComfyDialog
