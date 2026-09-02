1035b4e0fb3fbd914ad6733648a2e19533a8c51b
成田璃久 <rikunarita7669@gmail.com>
2026-09-03 01:32:08 +0900

    Add DropdownMenuTrigger component

---
+++ src/components/ui/dropdown-menu/DropdownMenuTrigger.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { DropdownMenuTriggerProps } from 'reka-ui'
+import { DropdownMenuTrigger } from 'reka-ui'
+
+const props = defineProps<DropdownMenuTriggerProps>()
+</script>
+
+<template>
+  <DropdownMenuTrigger v-bind="props">
+    <slot />
+  </DropdownMenuTrigger>
+</template>
