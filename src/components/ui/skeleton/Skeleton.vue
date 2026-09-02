2787d3255329ee13c6cebb2c5965d8908dd9e31c
成田璃久 <rikunarita7669@gmail.com>
2026-09-03 01:28:34 +0900

    Add Skeleton component with props support

---
+++ src/components/ui/skeleton/Skeleton.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { HTMLAttributes } from 'vue'
+import { cn } from 'utils/cn'
+
+interface SkeletonProps {
+  class?: HTMLAttributes['class']
+}
+
+const props = defineProps<SkeletonProps>()
+</script>
+
+<template>
+  <div :class="cn('animate-pulse rounded-mm-ctl bg-mm-surface-hover', props.class)" />
+</template>
