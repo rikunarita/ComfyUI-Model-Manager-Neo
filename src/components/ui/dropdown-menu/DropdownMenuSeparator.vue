33f0d2cfb90cbf8c5ffdd23e871dd4690b2cda3b
成田璃久 <rikunarita7669@gmail.com>
2026-09-03 01:33:45 +0900

    Add DropdownMenuSeparator component

---
+++ src/components/ui/dropdown-menu/DropdownMenuSeparator.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { DropdownMenuSeparatorProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { DropdownMenuSeparator } from 'reka-ui'
+import { cn } from 'utils/cn'
+
+const props = defineProps<DropdownMenuSeparatorProps & { class?: HTMLAttributes['class'] }>()
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <DropdownMenuSeparator v-bind="delegatedProps" :class="cn('-mx-1 my-1 h-px bg-mm-border', props.class)" />
+</template>
