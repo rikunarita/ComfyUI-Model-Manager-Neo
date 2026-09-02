66a5ffd99eac699074671b1b4d00f901b621b248
成田璃久 <rikunarita7669@gmail.com>
2026-09-03 00:50:06 +0900

    Add SelectSeparator component with props handling

---
+++ src/components/ui/select/SelectSeparator.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { SelectSeparatorProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { SelectSeparator } from 'reka-ui'
+import { cn } from 'utils/cn'
+
+const props = defineProps<SelectSeparatorProps & { class?: HTMLAttributes['class'] }>()
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <SelectSeparator v-bind="delegatedProps" :class="cn('-mx-1 my-1 h-px bg-mm-border', props.class)" />
+</template>
