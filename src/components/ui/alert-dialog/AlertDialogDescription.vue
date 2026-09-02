fff22bd31286d1415bd327e67b26068e40106b7e
成田璃久 <rikunarita7669@gmail.com>
2026-09-03 01:13:23 +0900

    Add AlertDialogDescription component

---
+++ src/components/ui/alert-dialog/AlertDialogDescription.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { AlertDialogDescriptionProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { AlertDialogDescription } from 'reka-ui'
+import { cn } from 'utils/cn'
+
+const props = defineProps<AlertDialogDescriptionProps & { class?: HTMLAttributes['class'] }>()
+const delegatedProps = reactiveOmit(props, 'class')
+</script>
+
+<template>
+  <AlertDialogDescription v-bind="delegatedProps" :class="cn('text-sm text-mm-muted-fg', props.class)">
+    <slot />
+  </AlertDialogDescription>
+</template>
