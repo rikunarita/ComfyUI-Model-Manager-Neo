4114e41950a18ee4844296708c9560d912a47c97
成田璃久 <rikunarita7669@gmail.com>
2026-09-03 01:33:20 +0900

    Add DropdownMenuLabel component

---
+++ src/components/ui/dropdown-menu/DropdownMenuLabel.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { DropdownMenuLabelProps } from 'reka-ui'
+import type { HTMLAttributes } from 'vue'
+import { reactiveOmit } from '@vueuse/core'
+import { DropdownMenuLabel } from 'reka-ui'
+import { cn } from 'utils/cn'
+
+const props = defineProps<DropdownMenuLabelProps & { class?: HTMLAttributes['class']; inset?: boolean }>()
+const delegatedProps = reactiveOmit(props, 'class', 'inset')
+</script>
+
+<template>
+  <DropdownMenuLabel
+    v-bind="delegatedProps"
+    :class="cn('px-2 py-1.5 text-sm font-semibold', inset && 'pl-8', props.class)"
+  >
+    <slot />
+  </DropdownMenuLabel>
+</template>
