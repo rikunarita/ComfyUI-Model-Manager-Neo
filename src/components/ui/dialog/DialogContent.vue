d388540891eabb2329f4a3fd9731af64b6ce9d22
成田璃久 <rikunarita7669@gmail.com>
2026-09-05 01:25:30 +0900

    Add forceMount prop to DialogContent component

--- src/components/ui/dialog/DialogContent.vue
+++ src/components/ui/dialog/DialogContent.vue
@@ -15,23 +15,30 @@ import DialogOverlay from './DialogOverlay.vue'
 defineOptions({ inheritAttrs: false })
 
 const props = withDefaults(
-  defineProps<DialogContentProps & { class?: HTMLAttributes['class']; showCloseButton?: boolean }>(),
-  { showCloseButton: true },
+  defineProps<
+    DialogContentProps & {
+      class?: HTMLAttributes['class']
+      showCloseButton?: boolean
+      forceMount?: boolean
+    }
+  >(),
+  { showCloseButton: true, forceMount: false },
 )
 const emits = defineEmits<DialogContentEmits>()
-const delegatedProps = reactiveOmit(props, 'class')
+const delegatedProps = reactiveOmit(props, 'class', 'forceMount')
 const forwarded = useForwardPropsEmits(delegatedProps, emits)
 </script>
 
 <template>
-  <DialogPortal>
-    <DialogOverlay />
+  <DialogPortal :force-mount="forceMount">
+    <DialogOverlay :class="forceMount && 'data-[state=closed]:hidden'" />
     <DialogContent
       v-bind="{ ...forwarded, ...$attrs }"
       :class="cn(
         'mm-glass fixed left-1/2 top-1/2 z-50 grid w-full max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 rounded-mm-dlg border border-mm-border p-6',
         'animate-in fade-in-0 zoom-in-95 duration-200',
         'data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95',
+        forceMount && 'data-[state=closed]:hidden',
         props.class,
       )"
     >
