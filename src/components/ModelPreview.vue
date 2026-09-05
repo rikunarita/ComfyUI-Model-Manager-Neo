fd2e7ef54b7616c447f2f7c4e0d939e252714bbd
成田璃久 <rikunarita7669@gmail.com>
2026-09-05 18:38:10 +0900

    Replace Carousel with custom slider implementation

--- src/components/ModelPreview.vue
+++ src/components/ModelPreview.vue
@@ -24,43 +24,38 @@
           :error="noPreviewContent"
         ></ResponseImage>
 
-        <Carousel
+        <!-- Carousel replacement: simple slider -->
+        <div
           v-if="defaultContent.length > 1"
           v-show="currentType === 'default'"
           class="absolute top-0 h-full w-full"
-          :value="defaultContent"
-          v-model:page="defaultContentPage"
-          :circular="true"
-          :show-navigators="true"
-          :show-indicators="false"
-          pt:contentcontainer:class="h-full"
-          pt:content:class="h-full"
-          pt:itemlist:class="h-full"
-          :prev-button-props="{
-            class: 'absolute left-4 z-10',
-            rounded: true,
-            severity: 'secondary',
-          }"
-          :next-button-props="{
-            class: 'absolute right-4 z-10',
-            rounded: true,
-            severity: 'secondary',
-          }"
         >
-          <template #item="slotProps">
+          <div class="h-full w-full">
             <div
-              v-if="isVideoUrl(slotProps.data)"
+              v-if="isVideoUrl(defaultContent[defaultContentPage])"
               class="h-full w-full p-1 hover:p-0"
             >
-              <PreviewVideo :src="slotProps.data" />
+              <PreviewVideo :src="defaultContent[defaultContentPage]" />
             </div>
             <ResponseImage
               v-else
-              :src="slotProps.data"
+              :src="defaultContent[defaultContentPage]"
               :error="noPreviewContent"
             ></ResponseImage>
-          </template>
-        </Carousel>
+          </div>
+          <button
+            class="absolute left-4 top-1/2 z-10 -translate-y-1/2 rounded-full bg-mm-surface/80 p-1 backdrop-blur-sm mm-transition hover:bg-mm-surface-hover"
+            @click="prevPage"
+          >
+            <ChevronLeft class="size-4" />
+          </button>
+          <button
+            class="absolute right-4 top-1/2 z-10 -translate-y-1/2 rounded-full bg-mm-surface/80 p-1 backdrop-blur-sm mm-transition hover:bg-mm-surface-hover"
+            @click="nextPage"
+          >
+            <ChevronRight class="size-4" />
+          </button>
+        </div>
       </div>
     </div>
 
@@ -75,10 +70,11 @@
         <Button
           v-for="type in typeOptions"
           :key="type"
-          :severity="currentType === type ? undefined : 'secondary'"
-          :label="$t(type)"
+          :variant="currentType === type ? 'default' : 'secondary'"
           @click="currentType = type"
-        ></Button>
+        >
+          {{ $t(type) }}
+        </Button>
       </div>
 
       <div v-show="currentType === 'network'">
@@ -105,15 +101,15 @@
 </template>
 
 <script setup lang="ts">
+import { ChevronLeft, ChevronRight } from '@lucide/vue'
 import PreviewVideo from 'components/PreviewVideo.vue'
 import ResponseFileUpload from 'components/ResponseFileUpload.vue'
 import ResponseImage from 'components/ResponseImage.vue'
 import ResponseInput from 'components/ResponseInput.vue'
+import { Button } from 'components/ui/button'
 import { useConfig } from 'hooks/config'
 import { useContainerQueries } from 'hooks/container'
 import { useModelPreview } from 'hooks/model'
-import Button from 'primevue/button'
-import Carousel from 'primevue/carousel'
 import { isVideoUrl } from 'utils/media'
 
 const editable = defineModel<boolean>('editable')
@@ -132,4 +128,15 @@ const {
 } = useModelPreview()
 
 const { $sm, $xl } = useContainerQueries()
+
+const prevPage = () => {
+  defaultContentPage.value =
+    (defaultContentPage.value - 1 + defaultContent.value.length) %
+    defaultContent.value.length
+}
+
+const nextPage = () => {
+  defaultContentPage.value =
+    (defaultContentPage.value + 1) % defaultContent.value.length
+}
 </script>
