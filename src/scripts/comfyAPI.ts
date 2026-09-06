a449cdfe08e50303a5be17f4e0e763dd157a28e5
成田璃久 <rikunarita7669@gmail.com>
2026-09-07 01:21:07 +0900

    Refactor exports in comfyAPI.ts

--- src/scripts/comfyAPI.ts
+++ src/scripts/comfyAPI.ts
@@ -2,7 +2,3 @@ export const app = window.comfyAPI.app.app
 export const api = window.comfyAPI.api.api
 
 export const $el = window.comfyAPI.ui.$el
-
-export const ComfyApp = window.comfyAPI.app as any
-export const ComfyButton = window.comfyAPI.button.ComfyButton
-export const ComfyDialog = window.comfyAPI.dialog.ComfyDialog
