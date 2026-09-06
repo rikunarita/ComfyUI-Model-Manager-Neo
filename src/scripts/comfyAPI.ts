6237050363e60e52ba361dc1cb5b570c2f75bc01
成田璃久 <rikunarita7669@gmail.com>
2026-09-07 02:11:11 +0900

    Add ComfyButton export from comfyAPI

--- src/scripts/comfyAPI.ts
+++ src/scripts/comfyAPI.ts
@@ -2,3 +2,5 @@ export const app = window.comfyAPI.app.app
 export const api = window.comfyAPI.api.api
 
 export const $el = window.comfyAPI.ui.$el
+
+export const ComfyButton = window.comfyAPI.button.ComfyButton
